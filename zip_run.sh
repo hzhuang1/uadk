25fe1e7b5585dcdbcac17e415a2cf0e83f6a4e3f
Haojian Zhuang <haojian.zhuang@linaro.org>
2021-06-08 14:06:11 +0800

    debug: clean script
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- zip_run.sh
+++ zip_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+#VALGRIND="valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all"
+#VALGRIND="valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s"
 #VALGRIND="valgrind --tool=memcheck --leak-check=yes"
 RM="sudo rm"
 CP="sudo cp"
@@ -249,25 +251,26 @@ hw_dfl_hw_ifl()
 	hw_blk_deflate origin /tmp/ori.gz gzip -b 8192
 	hw_blk_inflate /tmp/ori.gz origin gzip -b 8192
 	md5sum -c ori.md5
-	echo "Pass RANDOM data for hw compress and hw decompress"
+	echo "Pass RANDOM data for hw compress and hw decompress in BLOCK"
 
 	${RM} -f /tmp/ori.gz
 	hw_strm_deflate origin /tmp/ori.gz gzip -b 8192
 	hw_strm_inflate /tmp/ori.gz origin gzip -b 8192
 	md5sum -c ori.md5
+	echo "Pass RANDOM data for hw compress and hw decompress in STREAM"
 
 	# Use existed text file. It's not in alignment.
 	${RM} -f origin /tmp/ori.gz ori.md5
 	prepare_src_file $1
 	md5sum origin > ori.md5
 
-	hw_strm_deflate origin /tmp/ori.gz gzip -b 8192
-	hw_strm_inflate /tmp/ori.gz origin gzip -b 8192
+	hw_blk_deflate origin /tmp/ori.gz gzip -b 8192
+	hw_blk_inflate /tmp/ori.gz origin gzip -b 8192
 	md5sum -c ori.md5
 
 	${RM} -f /tmp/ori.gz
-	hw_blk_deflate origin /tmp/ori.gz gzip -b 8192
-	hw_blk_inflate /tmp/ori.gz origin gzip -b 8192
+	hw_strm_deflate origin /tmp/ori.gz gzip -b 8192
+	hw_strm_inflate /tmp/ori.gz origin gzip -b 8192
 	md5sum -c ori.md5
 }
 
@@ -275,89 +278,9 @@ if [ ! -z $1 ]; then
 	${HWZIP} --self
 	exit
 fi
-hw_dfl_sw_ifl /var/log/syslog
 sw_dfl_hw_ifl /var/log/syslog
-#hw_dfl_hw_ifl /var/log/syslog
-exit
+hw_dfl_sw_ifl /var/log/syslog
+hw_dfl_hw_ifl /var/log/syslog
 
-#echo "run without parameter"
-#sudo \
-#  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-#  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-#  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-#  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf
-
-echo "hardware compress gzip and software decompress"
-dd if=/dev/urandom of=origin bs=1M count=1 &> /dev/null
-md5sum origin
-md5sum origin > ori.md5
-${HWZIP} --in origin --out /tmp/ori.gz
-gunzip < /tmp/ori.gz > origin
-md5sum -c ori.md5
-md5sum origin
-exit
-
-#echo "start test"
-#sudo \
-#  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-#  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-#  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-#  test/sanity_test.sh
-
-echo "Start to compress Makefile with BLOCK"
-sudo \
-  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf --in Makefile --out /tmp/x.zip -b 8192 -m 0
-sudo \
-  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -d --in /tmp/x.zip --out /tmp/M -b 8192
-exit
-echo "Start to compress Makefile with STREAM"
-sudo \
-  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf --in Makefile --out /tmp/x.zip -l 1000 -b 8192 -S
-sudo \
-  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -d --in /tmp/x.zip --out /tmp/M -l 1000 -b 8192 -S -t 10 -m 1
-echo "Start to compress RANDOM DATA with BLOCK"
-for thread in 1 2 4 8 16
-do
-	sudo \
-	  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-	  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-	  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-	  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -z -t ${thread} -b 8192
-	sudo \
-	  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-	  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-	  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-	  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -d -t ${thread} -b 8192
-	sudo \
-	  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-	  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-	  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-	  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -z -t ${thread} -b 8192 -m 0
-	sudo \
-	  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-	  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-	  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-	  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -d -t ${thread} -b 8192 -m 0
-done
-exit
-
-sudo \
-  LD_LIBRARY_PATH=${LIB_ROOT}/${LIB_DIR} \
-  PATH=${LIB_ROOT}/${BIN_DIR}/:${PATH} \
-  C_INCLUDE_PATH=${LIB_ROOT}/${INC_DIR} \
-  ${LIB_ROOT}/${BIN_DIR}/zip_sva_perf --self
-
-
-  #${LIB_ROOT}/${BIN_DIR}/zip_sva_perf -s 0x1000 -b 0x1000 -k t
+# run self test
+${HWZIP} --self
